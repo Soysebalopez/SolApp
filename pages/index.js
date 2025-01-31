31debe86fdf89afa04c0cbdcd1f1a60a094e1acf
sebastian-simplistic <soysebalopez@gmail.com>
2025-01-31 19:29:48 -0300

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,21 @@
 import { useEffect } from 'react';
 import { useRouter } from 'next/router';
+import { auth } from '../lib/firebase';
 
 export default function Home() {
   const router = useRouter();
+
   useEffect(() => {
-    router.push('/dashboard');
-  }, []);
+    const unsubscribe = auth.onAuthStateChanged((user) => {
+      if (user) {
+        router.push('/dashboard');
+      } else {
+        router.push('/login'); // Asegúrate de tener una página de login
+      }
+    });
+
+    return () => unsubscribe();
+  }, [router]);
+
   return <p>Redirigiendo...</p>;
 }
